4f2fd6658ee1de11e25cf01f7a9f424df887f96e
mehta4tejas <mehta4tejas@gmail.com>
2020-07-02 12:06:27 +0530

    For Testing

--- SpringCore/src/main/java/org/tejas/SpringCore/First/App.java
+++ SpringCore/src/main/java/org/tejas/SpringCore/First/App.java
@@ -12,7 +12,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
         Circle circle = (Circle) beanFactory.getBean("circle");
         circle.display();
